de5d3248f6cb9499ddf5592dc5f900b1dc48cf07
Josué Carlos <josuecarlosos2@gmail.com>
2022-07-03 23:19:23 -0300

    application.properties

--- src/main/java/com/example/demo/domain/Camisa.java
+++ src/main/java/com/example/demo/domain/Camisa.java
@@ -3,11 +3,11 @@ package com.example.demo.domain;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 
 @Entity
 @AllArgsConstructor
